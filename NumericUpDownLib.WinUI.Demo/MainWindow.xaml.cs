d8ba2559c7efb73aba6569307be1b8bc28deaa6d
heartacker <heartacker@outlook.com>
2023-03-27 09:47:43 +0800

    界面

--- NumericUpDownLib.WinUI.Demo/MainWindow.xaml.cs
+++ NumericUpDownLib.WinUI.Demo/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
+using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -24,6 +25,7 @@ namespace NumericUpDownLib.WinUI.Demo
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
+    [INotifyPropertyChanged]
     public sealed partial class MainWindow : Window
     {
         public MainWindow()
@@ -31,9 +33,21 @@ namespace NumericUpDownLib.WinUI.Demo
             this.InitializeComponent();
         }
 
+
+        public byte lens = 5;
+        public byte Lens
+        {
+            get { return (byte)(lens); }
+            set
+            {
+                Lens = (byte)value;
+                OnPropertyChanged(nameof(lens));
+            }
+        }
         private void myButton_Click(object sender, RoutedEventArgs e)
         {
             myButton.Content = "Clicked";
+            bbb.Value = bbb.Value > 100 ? (byte)0 : (byte)(bbb.Value + 1);
         }
 
         private void NumberBox_ValueChanged(NumberBox sender, NumberBoxValueChangedEventArgs args)
