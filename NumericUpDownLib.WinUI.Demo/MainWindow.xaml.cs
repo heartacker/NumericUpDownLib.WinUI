cf4ec87f16a250e3282a8e429bbdbd2f2e93247b
heartacker <heartacker@outlook.com>
2023-03-28 21:22:18 +0800

    PP

--- NumericUpDownLib.WinUI.Demo/MainWindow.xaml.cs
+++ NumericUpDownLib.WinUI.Demo/MainWindow.xaml.cs
@@ -1,66 +1,15 @@
-// Copyright (c) Microsoft Corporation and Contributors.
-// Licensed under the MIT License.
+﻿using NumericUpDownLib.WinUI.Demo.Helpers;
 
-using CommunityToolkit.Mvvm.ComponentModel;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Globalization.NumberFormatting;
+namespace NumericUpDownLib.WinUI.Demo;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
-namespace NumericUpDownLib.WinUI.Demo
+public sealed partial class MainWindow : WindowEx
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    [INotifyPropertyChanged]
-    public sealed partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            this.InitializeComponent();
-        }
+        InitializeComponent();
 
-
-        public byte lens = 5;
-        public byte Lens
-        {
-            get { return (byte)(lens); }
-            set
-            {
-                Lens = (byte)value;
-                OnPropertyChanged(nameof(lens));
-            }
-        }
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
-            bbb.MaxValue = (byte)10;
-            bbb.MinValue = (byte)1;
-            bbb.Value = bbb.Value > 100 ? (byte)0 : (byte)(bbb.Value + 1);
-        }
-
-        private void NumberBox_ValueChanged(NumberBox sender, NumberBoxValueChangedEventArgs args)
-        {
-
-        }
-
-        private void myButton_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-
-        }
+        AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/WindowIcon.ico"));
+        Content = null;
+        Title = "AppDisplayName".GetLocalized();
     }
 }
